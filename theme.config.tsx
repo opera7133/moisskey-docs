8922e74b6f0a502593543ceb51bc136822fb2cb6
opera7133 <ainznino@pm.me>
2023-02-20 10:57:07 +0900

    init

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Moisskey Docs</span>,
+  project: {
+    link: 'https://github.com/opera7133/moisskey',
+  },
+  docsRepositoryBase: 'https://github.com/opera7133/moisskey-docs',
+  footer: {
+    text: 'Nextra Docs Template',
+  },
+}
+
+export default config
