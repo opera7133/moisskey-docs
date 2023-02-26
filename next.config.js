922475448843c303517a33eaf7d02a14cba982ad
wamο <ainznino@pm.me>
2023-02-26 13:05:42 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-})
+});
 
 module.exports = {
   ...withNextra(),
   images: {
     unoptimized: true,
   },
+  reactStrictMode: true,
+  swcMinify: true,
+  trailingSlash: true,
 };
